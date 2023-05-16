14853588ea97eaee2f6891e84df5da52c9912d0b
Vladilshk <vova.ilyshenko2016@gmail.com>
2023-05-16 11:04:24 +0300

    Initial commit

---
+++ src/test/java/ru/vovai/telrossofttesttask/TelrosSoftTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vovai.telrossofttesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelrosSoftTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
